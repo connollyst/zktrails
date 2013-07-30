2b6dfbcbeaa288499b2a9b400e676d03a34836ef
Sean Connolly <connolly.st@gmail.com>
2013-07-30 12:59:20 -0400

    Fixed Trail rendering to support Trailitems.

--- src/main/resources/web/js/zul/trail/mold/zktrail.js
+++ src/main/resources/web/js/zul/trail/mold/zktrail.js
@@ -1,5 +1,8 @@
 function (out) {
 
-    out.push('<ol ', this.domAttrs_(), '></ol>');
+    out.push('<ol ', this.domAttrs_(), '>')
+    for (var w = this.firstChild; w; w = w.nextSibling)
+        w.redraw(out);
+    out.push('</ol>');
 
 }
