3b9f3c4c82e3dc03827d98cf1b48b162d89a6cf2
Sean Connolly <connolly.st@gmail.com>
2013-07-30 13:37:30 -0400

    Added support for image direction (normal & reverse)

--- src/main/java/com/quane/zkoss/TrailItem.java
+++ src/main/java/com/quane/zkoss/TrailItem.java
@@ -1,5 +1,10 @@
 package com.quane.zkoss;
 
+import java.io.IOException;
+
+import org.zkoss.lang.Objects;
+import org.zkoss.zk.ui.WrongValueException;
+import org.zkoss.zk.ui.sys.ContentRenderer;
 import org.zkoss.zul.impl.LabelImageElement;
 
 /**
@@ -8,5 +13,57 @@ import org.zkoss.zul.impl.LabelImageElement;
  * @author Sean Connolly
  */
 public class TrailItem extends LabelImageElement {
+
+	private static final String NORMAL = "normal";
+	private static final String REVERSE = "reverse";
+
+	private String _dir = NORMAL;
+
+	/**
+	 * Returns the direction.<br/>
+	 * Default: "normal".
+	 */
+	public String getDir() {
+		return _dir;
+	}
+
+	/**
+	 * Sets the layout direction; the location of the image to the label.<br/>
+	 * Valid values are {@code normal} and {@code reverse}.
+	 *
+	 * @param dir
+	 *            either "normal" or "reverse".
+	 * @throws WrongValueException
+	 *             if an invalid direction is set
+	 */
+	public void setDir(String dir) throws WrongValueException {
+		if (!isValidDir(dir)) {
+			throw new WrongValueException(dir);
+		}
+		if (!Objects.equals(_dir, dir)) {
+			_dir = dir;
+			smartUpdateDir();
+		}
+	}
+
+	private boolean isValidDir(String dir) {
+		return NORMAL.equals(dir) || REVERSE.equals(dir);
+	}
+
+	private void smartUpdateDir() {
+		smartUpdate("dir", _dir);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void renderProperties(ContentRenderer renderer)
+			throws IOException {
+		super.renderProperties(renderer);
+		if (!NORMAL.equals(_dir)) {
+			render(renderer, "dir", _dir);
+		}
+	}
 
 }
--- src/main/resources/web/js/zul/trail/TrailItem.js
+++ src/main/resources/web/js/zul/trail/TrailItem.js
@@ -1,8 +1,19 @@
 zul.trail.TrailItem = zk.$extends(zul.LabelImageWidget, {
 
+    _dir: "normal",
 
     $define: {
-
+        /** Returns the direction.
+         * <p>Default: "normal".
+         * @return String
+         */
+        /** Sets the direction.
+         * @param String dir either "normal" or "reverse".
+         */
+        dir: _zkf = function () {
+            var n = this.$n();
+            if (n) n.innerHTML = this.domContent_();
+        }
     },
 
     // super//
@@ -15,7 +26,7 @@ zul.trail.TrailItem = zk.$extends(zul.LabelImageWidget, {
         this.$supers(zul.trail.TrailItem, 'unbind_', arguments);
     },
     domContent_: function () {
-        console.log ( 'getting dom content' );
+        console.log ( 'getting dom content: ' + this.getDir());
         var label = zUtl.encodeXML(this.getLabel()), img = this.getImage();
         if (!img) {
             return label;
