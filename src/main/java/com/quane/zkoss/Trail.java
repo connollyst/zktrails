97705ca2cb79c5754a92f7360f7672c5f933febc
Sean Connolly <connolly.st@gmail.com>
2013-07-30 12:58:48 -0400

    Extended ZK Components.

--- src/main/java/com/quane/zkoss/Trail.java
+++ src/main/java/com/quane/zkoss/Trail.java
@@ -1,10 +1,12 @@
 package com.quane.zkoss;
 
+import org.zkoss.zul.Hlayout;
+
 /**
  * A breadcrumb trail is a set of steps that can take the user back home from
  * their current location, through the sequence of steps that led them here.
  *
  * @author Sean Connolly
  */
-public class Trail {
+public class Trail extends Hlayout {
 }
--- src/main/java/com/quane/zkoss/TrailItem.java
+++ src/main/java/com/quane/zkoss/TrailItem.java
@@ -1,9 +1,12 @@
 package com.quane.zkoss;
 
+import org.zkoss.zul.Span;
+
 /**
  * A breadcrumb represents a single step on the trail back home.
  *
  * @author Sean Connolly
  */
-public class TrailItem {
+public class TrailItem extends Span {
+
 }
